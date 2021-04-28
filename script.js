476039ecf3f0ab740085504263114914f6334d29
Luciano Oyarzabal <77352887+KeyCress@users.noreply.github.com>
2021-04-28 19:35:15 -0300

    Fixed some typo
    From 'likes' to 'like' -.-

--- script.js
+++ script.js
@@ -9,16 +9,16 @@
 // likes ["Alex", "Jacob", "Mark", "Max"] -- must be "Alex, Jacob and 2 others like this"
 
 function likes(names) {
-	// TODO
+	//TODO
 	if (names.length === 0) {
 		return `no one likes this`;
 	} else if (names.length === 1) {
 		return `${names[0]} likes this`;
 	} else if (names.length === 2) {
-		return `${names[0]} and ${names[1]} likes this`;
+		return `${names[0]} and ${names[1]} like this`;
 	} else if (names.length === 3) {
-		return `${names[0]}, ${names[1]} and ${names[2]} likes this`;
+		return `${names[0]}, ${names[1]} and ${names[2]} like this`;
 	} else {
-		return `${names[0]}, ${names[1]} and ${names.length - 2} others likes this`;
+		return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
 	}
 }
